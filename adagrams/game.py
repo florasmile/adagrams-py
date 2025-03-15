af2c0cb0914a7a54406e3f9a5c5fcc7a82fd5200
dehuihu <dehuihu@gmail.com>
2025-03-15 07:38:50 -0700

    Finished wave_03 and passed test_03

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,6 +28,16 @@ LETTER_POOL = {
     'Y': 2, 
     'Z': 1
 }
+
+LETTER_VALUES = {
+    'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+    'D': 2, 'G': 2,
+    'B': 3, 'C': 3, 'M': 3, 'P': 3,
+    'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+    'K': 5,
+    'J': 8, 'X': 8,
+    'Q': 10, 'Z': 10
+}
 def draw_letters():
     """
     build a hand of 10 letters for the user
@@ -79,7 +89,14 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    # create LETTER_VALUES as a global variable outside (see top)
+    # iterate through word string, find points for each char and add them up, check length for extra points
+    score = 0
+    for char in word.upper():
+        score += LETTER_VALUES[char]
+    if len(word) in [7, 8, 9, 10]:
+        score += 8
+    return score
 
 def get_highest_word_score(word_list):
     pass
